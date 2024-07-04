4019b9ba8e1d82f51bd726f3c1050aab5bd2ff3d
Moiz-25 <160418178+Moiz-25@users.noreply.github.com>
2024-07-04 07:28:24 +0000

    Applied Bootstrap and Updated HTML file

--- app.py
+++ app.py
@@ -2,10 +2,37 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [{
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Faisalabad, Pakistan',
+    'salary': 'Rs. 200,000'
+}, {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Lahore, Pakistan',
+    'salary': 'Rs. 300,000'
+}, {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Remote',
+    'salary': 'Rs. 150,000'
+}, {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'San Francisco, USA',
+    'salary': '$80,000'
+}, {
+    'id': 5,
+    'title': 'Full Stack Engineer',
+    'location': 'London, UK',
+    'salary': '$100,000'
+}]
+
 
 @app.route("/")
 def hello():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, company_name='Quantum')
 
 
 if __name__ == "__main__":
