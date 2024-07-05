f553bf605c7508a88884a23fbafdfe3026209c07
Moiz-25 <160418178+Moiz-25@users.noreply.github.com>
2024-07-05 00:59:56 +0000

    Added Dynamic Data and API route

--- app.py
+++ app.py
@@ -1,39 +1,48 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-JOBS = [{
-    'id': 1,
-    'title': 'Data Analyst',
-    'location': 'Faisalabad, Pakistan',
-    'salary': 'Rs. 200,000'
-}, {
-    'id': 2,
-    'title': 'Data Scientist',
-    'location': 'Lahore, Pakistan',
-    'salary': 'Rs. 300,000'
-}, {
-    'id': 3,
-    'title': 'Frontend Engineer',
-    'location': 'Remote',
-    'salary': 'Rs. 150,000'
-}, {
-    'id': 4,
-    'title': 'Backend Engineer',
-    'location': 'San Francisco, USA',
-    'salary': '$80,000'
-}, {
-    'id': 5,
-    'title': 'Full Stack Engineer',
-    'location': 'London, UK',
-    'salary': '$100,000'
-}]
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Faisalabad, Pakistan',
+        'salary': 'Rs. 200,000'
+    },
+    {
+        'id': 2,
+        'title': 'Data Scientist',
+        'location': 'Lahore, Pakistan',
+        'salary': 'Rs. 300,000'
+    },
+    {
+        'id': 3,
+        'title': 'Frontend Engineer',
+        'location': 'Remote',
+        'salary': 'Rs. 150,000'
+    },
+    {
+        'id': 4,
+        'title': 'Backend Engineer',
+        'location': 'San Francisco, USA',
+        'salary': '$80,000'
+    },
+    {
+        'id': 5,
+        'title': 'Full Stack Engineer',
+        'location': 'London, UK',
+        'salary': '$100,000'
+    }
+]
 
 
 @app.route("/")
 def hello():
-  return render_template('home.html', jobs=JOBS, company_name='Quantum')
+    return render_template('home.html', jobs=JOBS, company_name='Quantum')
 
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(JOBS)
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
